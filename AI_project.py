51d1b37f00afd8253f8cd0fb1c30eff0f4b7ca23
Timofei Kuzin <timkkuz@gmail.com>
2024-11-04 00:39:40 +0100

    Small changes

--- AI_project.py
+++ AI_project.py
@@ -3,6 +3,7 @@ import math
 import json
 import subprocess
 import sys
+import time
 
 # Function that is used to put all data: centers of clusters and clusters to json file.
 # Because PyPy is not compatible to matplotlib we have to put all computed data to file.
@@ -13,7 +14,7 @@ def save_clusters_to_json(clusters, centers, filename="clusters_data.json"):
 
 # Function to create first random 20 points
 def first_points(points):
-    # creating set of bods (set will provide us with unique starting bods)
+    # creating set of points (set will provide us with unique starting points)
     points_set = set()
     while len(points_set) < 20:
         x = random.randint(-5000, 5000)
@@ -27,10 +28,10 @@ def create_points(number_points, points):
         base_point = random.choice(points)
         # In this part we check the X-axis
         if base_point[0] > 4900:
-            #too close to right border
+            # too close to right border
             x = random.randint(base_point[0] - 100, 5000)
         elif base_point[0] < -4900:
-            #too close to left border
+            # too close to left border
             x = random.randint(-5000, base_point[0] + 100)
         else:
             x = random.randint(base_point[0] - 100, base_point[0] + 100)
@@ -72,7 +73,7 @@ def compute_medoids(medoids, clusters):
         distances_min = float('inf')
         medoid = None
         for candidate in cluster:
-            distances_sum = sum(dist(candidate[0], candidate[1], bod[0], bod[1]) for bod in cluster)
+            distances_sum = sum(dist(candidate[0], candidate[1], point[0], point[1]) for point in cluster)
             if distances_sum < distances_min:
                 distances_min = distances_sum
                 medoid = candidate
@@ -87,10 +88,13 @@ def divisive_clustering_kmeans(points, max_clusters, max_iterations=100):
     while len(clusters) < max_clusters:
 
         clusters_average_dist = []
+        # This is an optimisation for divisive algorithm.
+        # This block of code will choose the cluster with the biggest average distance from
+        # all points to the current centroid
         for centroid, cluster in zip(centroids, clusters):
             total_distance = 0
-            for bod in cluster:
-                total_distance += dist(bod[0], bod[1], centroid[0], centroid[1])
+            for point in cluster:
+                total_distance += dist(point[0], point[1], centroid[0], centroid[1])
             average_distance = total_distance / len(cluster)
             clusters_average_dist.append(average_distance)
         split_cluster_idx = clusters_average_dist.index(max(clusters_average_dist))
@@ -101,10 +105,8 @@ def divisive_clustering_kmeans(points, max_clusters, max_iterations=100):
         if len(split_cluster) < 2:
             break
 
-        # Apply custom k-means with k=2 to split the largest cluster
-        cluster_number = 2
-        new_clusters, new_centroids = clusterization_k_means(split_cluster, "centroid", cluster_number,
-                                                             max_iterations)
+        # Call k-means clusterization with k=2 to split the largest cluster
+        new_clusters, new_centroids = clusterization_k_means(split_cluster, "centroid", 2)
 
         # Replace the largest cluster with the two new clusters
         clusters[split_cluster_idx] = new_clusters[0]
@@ -120,29 +122,29 @@ def divisive_clustering_kmeans(points, max_clusters, max_iterations=100):
 # Function to evaluate created clusters
 def evaluation(clusters, centroids):
     i = 0
-    sum = 0
+    all_points_proc = 0
     for centroid, cluster in zip(centroids, clusters):
         i += 1
         total_distance = 0
-        for bod in cluster:
-            total_distance += dist(bod[0], bod[1], centroid[0], centroid[1])
+        for point in cluster:
+            total_distance += dist(point[0], point[1], centroid[0], centroid[1])
         average_distance = total_distance / len(cluster)
-        bods_num = len(cluster)
-        sum += bods_num
+        points_num = len(cluster)
+        all_points_proc += points_num
         if average_distance <= 500:
-            print("Cluster " + str(i) + " success!! With average distance " + str(average_distance) + " and with " + str(bods_num) + " bods")
+            print("Cluster " + str(i) + " success!! With average distance " + str(average_distance) + " and with " + str(points_num) + " points")
         else:
-            print("Cluster " + str(i) + " failure :( With average distance " + str(average_distance) + " and with " + str(bods_num) + " bods")
-    print("Total number of points that were processed: " + str(sum))
+            print("Cluster " + str(i) + " failure :( With average distance " + str(average_distance) + " and with " + str(points_num) + " points")
+    print("Total number of points that were processed: " + str(all_points_proc))
 
 
-def clusterization_k_means(points, mode, cluster_number, max_iterations):
+def clusterization_k_means(points, mode, cluster_number):
     # Initializing first centers as first points in 'points'
     clusters_centers = points[:cluster_number]
-    for _ in range (max_iterations):
+    while True:
         clusters = [[] for _ in range(cluster_number)]
         # appending points into clusters based on centroids
-        # (in first iteration based on first bods)
+        # (in first iteration based on first points)
         for point in points:
             distances = []
             # Here we are finding the nearest center to some point so we can attach the point to center of its group
@@ -161,11 +163,15 @@ def clusterization_k_means(points, mode, cluster_number, max_iterations):
         clusters_centers = new_centers
     return clusters, clusters_centers
 
+def process_clusterization(clusters, centers, alg_name):
+    filename = "clusters_data.json"
+    save_clusters_to_json(clusters, centers, filename=filename)
+    subprocess.Popen(["python3", "Visual_module.py", filename, alg_name])
+    evaluation(clusters, centers)
 
 def main():
-    max_clusters = 18
-    cluster_number = 19
-    max_iterations = 50
+    cluster_number_div = 18
+    cluster_number_k_means = 18
     points = []
     first_points(points)
     input_points = 40000
@@ -174,35 +180,40 @@ def main():
           "put 'C' for K-means with centroid\n"
           "put 'D' for Divisive clustering \n"
           "If you want to use all three then put MCD")
-    m = sys.stdin.readline().strip()
+    mode_alg = sys.stdin.readline().strip()
     create_points(input_points, points)
-    if m == 'M':
-        clusters, centers = clusterization_k_means(points, "medoid", cluster_number, max_iterations)
-    elif m == 'C':
-        clusters, centers = clusterization_k_means(points, "centroid", cluster_number, max_iterations)
-    elif m == 'D':
-        clusters, centers = divisive_clustering_kmeans(points, max_clusters)
-    elif m == "MCD":
-        clusters, centers = clusterization_k_means(points, "medoid", cluster_number, max_iterations)
-        save_clusters_to_json(clusters, centers, filename="clusters_data.json")
-        subprocess.Popen(["python3", "Visual_module.py", "clusters_data.json"])
-        evaluation(clusters, centers)
+    if mode_alg == 'M':
+        clusters, centers = clusterization_k_means(points, "medoid", cluster_number_k_means)
+        process_clusterization(clusters, centers, "K - means with medoids")
+    elif mode_alg == 'C':
+        clusters, centers = clusterization_k_means(points, "centroid", cluster_number_k_means)
+        process_clusterization(clusters, centers, "K - means with centroids")
+    elif mode_alg == 'D':
+        clusters, centers = divisive_clustering_kmeans(points, cluster_number_div)
+        process_clusterization(clusters, centers, "Divisive clusterization with centroids")
+    elif mode_alg == "MCD":
+        print("K-means with medoids with " + str(cluster_number_k_means) + " medoids:")
+        clusters_medoids, medoids = clusterization_k_means(points, "medoid", cluster_number_k_means)
+        evaluation(clusters_medoids, medoids)
+        print("K-means with centroids with " + str(cluster_number_k_means) + " centroids:")
+        clusters_centroids, centroids = clusterization_k_means(points, "centroid", cluster_number_k_means)
+        evaluation(clusters_centroids, centroids)
+        print("Divisive with centroids with " + str(cluster_number_div) + " centroids:")
+        clusters_divisive, centers_divisive = divisive_clustering_kmeans(points, cluster_number_div)
+        evaluation(clusters_divisive, centers_divisive)
+        data = {
+            "K-means medoids": {"clusters": clusters_medoids, "centers": medoids},
+            "K-means centroids": {"clusters": clusters_centroids, "centers": centroids},
+            "Divisive clustering": {"clusters": clusters_divisive, "centers": centers_divisive}
+        }
+        filename = "all_algorithms_data.json"
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+        subprocess.Popen(["python3", "Visual_module.py", filename, "All algorithms"])
 
-        clusters, centers = clusterization_k_means(points, "centroid", cluster_number, max_iterations)
-        save_clusters_to_json(clusters, centers, filename="clusters_data.json")
-        subprocess.Popen(["python3", "Visual_module.py", "clusters_data.json"])
-        evaluation(clusters, centers)
-
-        clusters, centers = divisive_clustering_kmeans(points, max_clusters)
-        save_clusters_to_json(clusters, centers, filename="clusters_data.json")
-        subprocess.Popen(["python3", "Visual_module.py", "clusters_data.json"])
-        evaluation(clusters, centers)
     else:
         print("Input error")
         exit(1)
-    save_clusters_to_json(clusters, centers, filename="clusters_data.json")
-    subprocess.Popen(["python3", "Visual_module.py", "clusters_data.json"])
-    evaluation(clusters, centers)
 
 if __name__ == "__main__":
     main()
--- Visual_module.py
+++ Visual_module.py
@@ -2,39 +2,71 @@ import matplotlib.pyplot as plt
 import sys
 import json
 
-def plot_clusters(clusters, centroids):
-    plt.figure(figsize=(10, 10), dpi=120)  # Увеличиваем размер и разрешение графика
-    colors = plt.get_cmap("tab20", len(clusters))  # палитра для 20 кластеров
+# Was written by chatGPT
+def plot_algorithms(data):
+    fig, axs = plt.subplots(1, 3, figsize=(18, 6), dpi=120)  # Create a figure with 3 subplots for each algorithm
 
-    for i, cluster in enumerate(clusters):
-        if cluster:
+    colors = plt.get_cmap("tab20")  # Use a colormap with 20 distinct colors
+
+    for i, (alg_name, alg_data) in enumerate(data.items()):
+        clusters = alg_data["clusters"]
+        centroids = alg_data["centers"]
+
+        # Select the subplot for the current algorithm
+        ax = axs[i]
+        ax.set_title(alg_name)  # Set the title to the algorithm's name
+        ax.set_aspect('equal', 'box')
+
+        # Plot cluster points for the current algorithm
+        for j, cluster in enumerate(clusters):
             x_vals, y_vals = zip(*cluster)
-            plt.scatter(x_vals, y_vals, color=colors(i), label=f"Cluster {i + 1}", marker='o', s=1)
-        else:
-            continue
+            color = colors(j % 20)  # Apply a unique color for each cluster
+            ax.scatter(x_vals, y_vals, color=color, marker='o', s=1, alpha=0.6)
 
-    # Рисуем центроиды
+        # Plot centroids for the current algorithm
+        centroid_x, centroid_y = zip(*centroids)
+        ax.scatter(centroid_x, centroid_y, color='black', marker='o', s=5)
+
+        # Configure axis limits and grid
+        ax.set_xlim(-5000, 5000)
+        ax.set_ylim(-5000, 5000)
+        ax.grid(True)
+
+    plt.suptitle("Comparison of Clustering Algorithms")  # Set the main title for all subplots
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust layout for better spacing
+    plt.show()
+
+
+def plot_clusters(clusters, centroids, alg_name):
+    plt.figure(figsize=(10, 10), dpi=120)
+    colors = plt.get_cmap("tab20")
+    # Drawing clusters with their own colors
+    for i, cluster in enumerate(clusters):
+        x_vals, y_vals = zip(*cluster)
+        plt.scatter(x_vals, y_vals, color=colors(i), marker='o', s=1)
+    # Drawing centroids
     centroid_x, centroid_y = zip(*centroids)
-    plt.scatter(centroid_x, centroid_y, c='black', marker='o', s=10, label="Centroids")  # увеличиваем центроиды
-
+    plt.scatter(centroid_x, centroid_y, c='black', marker='o', s=10)
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
-    plt.title("K-means Medoids")
+    plt.title("Result of " + str(alg_name))
     plt.xlim(-5000, 5000)
     plt.ylim(-5000, 5000)
     plt.grid(True)
     plt.show()
 
 def main():
-    # Получаем путь к JSON-файлу из аргументов командной строки
+    # Getting the path to the JSON file
     filename = sys.argv[1]
+    alg_name = sys.argv[2]
     with open(filename, 'r') as f:
         data = json.load(f)
-
-    clusters = data["clusters"]
-    centroids = data["centers"]
-
-    plot_clusters(clusters, centroids)
+    if filename == "all_algorithms_data.json":
+        plot_algorithms(data)
+    else:
+        clusters = data["clusters"]
+        centroids = data["centers"]
+        plot_clusters(clusters, centroids, alg_name)
 
 if __name__ == "__main__":
     main()
